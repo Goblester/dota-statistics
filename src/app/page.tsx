d20e095b2525206cf94db5b403c57524c4564fa3
Goblester <goblin829@gmail.com>
2023-07-29 15:16:48 +0300

    changed link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,17 @@ import {fetchHeroes} from "@/api";
 import {HERO_IMAGES} from "@/constants/heroImages";
 import {ATTRIBUTES} from "@/constants/attributes";
 
+/**
+ * replaces spaces and dashes in strings
+ * @param {string} name - name
+ * @return {string} - trimmed link
+ */
+const getLinkName = (name: string): string => {
+    return name
+        .replaceAll(' ', '')
+        .replaceAll('-', '')
+        .toLowerCase();
+}
 
 export default async function Home() {
 
@@ -20,7 +31,7 @@ export default async function Home() {
                     return (
                         <li key={hero.id}>
                             <Link
-                                href={`/heroes/${hero.localized_name}`}>
+                                href={`/heroes/${getLinkName(hero.localized_name)}`}>
                                 <div
                                     style={{backgroundImage: `url(${HERO_IMAGES[hero.id].src})`}}
                                     className="group relative w-56 h-32 dark:bg-slate-800 bg-no-repeat bg-cover bg-center hover:scale-125 hover:z-10 duration-300 overflow-hidden shadow-md
@@ -28,7 +39,8 @@ export default async function Home() {
                                       <span
                                           className="flex absolute uppercase top-full left-1/2 w-full text-lg -translate-x-1/2 group-hover:-translate-y-8 duration-300 z-10">
                                 <span className="mx-2">
-                                    <Image src={ATTRIBUTES[hero.primary_attr].src} width={26} height={26}/>
+                                    <Image src={ATTRIBUTES[hero.primary_attr].src} width={26} height={26}
+                                           role="presentation" alt=""/>
                                 </span>
                                           {hero.localized_name}
                                  </span>
