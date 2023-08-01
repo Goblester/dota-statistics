94e8c9fec941f3ed43294c6e79aa672ffbdd10c7
Goblester <goblin829@gmail.com>
2023-08-01 11:55:03 +0300

    add jest and testing-library/react

---
+++ src/components/heroes/Heroes.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const Heroes: React.FC = () => {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
---
+++ src/components/heroes/heroes.test.tsx
@@ -0,0 +1,8 @@
+import {render} from "@testing-library/react";
+import {Heroes} from "@/components/heroes/Heroes";
+
+describe('<Heroes/>', () => {
+    it('should', () => {
+        render(<Heroes />);
+    });
+});
