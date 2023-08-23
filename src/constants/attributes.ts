7852e290ab7f4126a9bf3c4d4d6a5d0904b86235
Goblester <goblin829@gmail.com>
2023-08-23 20:39:47 +0300

    add attribute filter buttons with styling

--- src/constants/attributes.ts
+++ src/constants/attributes.ts
@@ -2,6 +2,10 @@ import universal from './../assets/heroes/attributes/hero_universal.png';
 import strength from './../assets/heroes/attributes/hero_strength.png';
 import agility from './../assets/heroes/attributes/hero_agility.png';
 import intelligence from './../assets/heroes/attributes/hero_intelligence.png';
+import filterUniversal from './../assets/heroes/attributes/filter-uni.png';
+import filterStrength from './../assets/heroes/attributes/filter-str.png';
+import filterAgility from './../assets/heroes/attributes/filter-agi.png';
+import filterIntelligence from './../assets/heroes/attributes/filter-int.png';
 
 type AttributesType = {
     [key:string]: { src: string };
@@ -12,4 +16,11 @@ export const ATTRIBUTES: AttributesType = {
     str: strength,
     agi: agility,
     int: intelligence,
-} as const;
+};
+
+export const FILTER_ATTRIBUTES: AttributesType = {
+    all: filterUniversal,
+    str: filterStrength,
+    agi: filterAgility,
+    int: filterIntelligence,
+};
--- src/features/filter/filter.tsx
+++ src/features/filter/filter.tsx
@@ -1,21 +1,29 @@
 "use client";
-import React from "react";
-import {ATTRIBUTES} from "@/constants/attributes";
+import React, {useState} from "react";
+import {FILTER_ATTRIBUTES} from "@/constants/attributes";
 import {SearchIcon} from "@/components/icons/searchIcon";
 
 
-const ATTRIBUTE_BUTTONS = ['str', 'agi', 'int', 'all'];
+const ATTRIBUTE_BUTTONS: AttributeType[] = ['str', 'agi', 'int', 'all'];
+
+type AttributeType = 'str' | 'agi' | 'int' | 'all' | '';
 
 
 export const Filter: React.FC = () => {
+    const [activeAttribute, setActiveAttribute] = useState<AttributeType>("");
+
     return (
-        <div className="flex justify-between w-full p-1 mb-10 bg-gradient-to-l from-gray-500 to-gray-700 border-black border items-center rounded-sm shadow-md">
-            <div className="flex">
-                <span>ATTRIBUTE</span>
-                <div className="flex">
+        <div
+            className="flex justify-between w-full p-1 mb-10 bg-gradient-to-l from-gray-500 to-gray-700 border-black border items-center rounded-sm shadow-md">
+            <div className="flex items-center">
+                <span className="ml-10">ATTRIBUTE</span>
+                <div className="flex gap-1 ml-10">
                     {ATTRIBUTE_BUTTONS.map(name => (
-                        <button key={name} aria-label={`attribute ${name}`} className="w-16 h-16"
-                                style={{backgroundImage: `url(${ATTRIBUTES[name].src})`}}/>
+                        <button key={name} aria-label={`attribute ${name}`}
+                                onClick={() => setActiveAttribute((value) => value === name ? "" : name)}
+                                className={`w-11 h-8 bg-no-repeat bg-cover bg-center -ml-2 saturate-0 brightness-50 transition duration-100
+                                ${activeAttribute === name && 'saturate-100 brightness-100'}`}
+                                style={{backgroundImage: `url(${FILTER_ATTRIBUTES[name].src})`}}/>
                     ))}
                 </div>
             </div>
@@ -26,7 +34,7 @@ export const Filter: React.FC = () => {
                 <button>3</button>
             </div>
             <div className="relative flex bg-gray-700 h-12 p-2 pl-12">
-                <SearchIcon size={26} className="absolute top-3 left-3 fill-gray-500" />
+                <SearchIcon size={26} className="absolute top-3 left-3 fill-gray-500"/>
                 <input className="px-4 h-8 bg-gray-500 outline-none border-none bg-gray-700 focus:bg-gray-500"/>
             </div>
         </div>
