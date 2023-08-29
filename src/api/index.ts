f1082008688dfafeabfe98ad6e23a78cafbd637c
Goblester <goblin829@gmail.com>
2023-08-29 21:28:40 +0300

    add JSdoc for api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,10 @@ import {HeroDataType, HeroStatsType} from "@/types";
 
 const baseUrl = 'https://api.opendota.com/api';
 
-
+/**
+ * gets all heroes short info
+ * @return {Promise<HeroDataType>} - Heres short data
+ */
 export const fetchHeroes = async (): Promise<HeroDataType[]> => {
     try {
         const response = await fetch(`${baseUrl}/heroes`);
@@ -11,7 +14,11 @@ export const fetchHeroes = async (): Promise<HeroDataType[]> => {
         console.error(e);
     }
 };
-
+/**
+ * gets all heroes stats and returns hero data
+ * @param id - hero id
+ * @return {Promise<HeroStatsType>} - Hero data
+ */
 export const fetchHero = async (id: number): Promise<HeroStatsType> => {
     try {
         const response = await fetch(`${baseUrl}/heroStats`);
