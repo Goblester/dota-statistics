ce2ea65bf9d657b52b9c954ad0ad0e8a117f5ee6
Goblester <goblin829@gmail.com>
2023-09-03 21:55:21 +0300

    write todo tests

---
+++ src/features/filter/filter.test.tsx
@@ -0,0 +1,8 @@
+
+
+
+describe('<Filter />', () => {
+    it.todo('should change attribute filter on attribute buttons click');
+    it.todo('should change complexity filter on complexiry buttons click');
+    it.todo('should change search value on input typing');
+});
---
+++ src/store/filter/index.test.ts
@@ -0,0 +1,16 @@
+
+
+
+describe('setAttribute', () => {
+    it.todo('should set attribute');
+    it.todo('should reset attribute if new value = current value');
+});
+
+describe('setComplexity', () => {
+    it.todo('should set complexity');
+    it.todo('should reset complexity if new value = current value');
+});
+
+describe('changeSearch', () => {
+    it.todo('should change search');
+});
